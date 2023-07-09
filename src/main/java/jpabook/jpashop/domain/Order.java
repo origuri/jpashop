a13cf28495cfa471438bce1f46e2a23ab21d3224
origuri <dlcksrb7@naver.com>
2023-07-09 15:12:45 +0900

    2023.7.9 - 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -49,4 +49,48 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //== 생성 메서드 ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //== 비즈니스 로직 == //
+    /*
+    *  주문 취소
+    * */
+    public void cancel(){
+        // 이미 배송 완료가 되었으면 취소 불가하게
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("배송완료가 된 제품은 취소가 불가능 합니다");
+        }
+        // 배송 완료가 아니면 버튼을 취소 가능 상태니까 취소로 변경한다.
+        this.setStatus(OrderStatus.CANCEL);
+
+        // 취소를 누르면 내가 시켰던 모든 아이템이 취소가 되야한다.
+        for(OrderItem orderItem : this.orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //== 조회 로직 ==//
+    /*
+     *  전체 주문 가격 조회
+     * */
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        // 사과 3개 바나나 2개를 샀다고 하면 사과 *3 + 바나나 *2 해야 총 값이 나옴.
+        for(OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,27 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+    //== 생성 메소드==//
+    // 가격을 아이템 엔티티 꺼를 사용안하는 이유는 할인이나 쿠폰 사용할 수 있기 때문
+    public static OrderItem createOrderItem(Item item , int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // 내가 산 아이템은 재고수량이 줄어들어야 함.
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //== 비즈니스 로직==//
+    public void cancel(){
+        // 취소 했으니까 수량을 원복 해줘야 한다.
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        // 주문 가격과 주문 수량을 곱해야 하기 때문
+        return getOrderPrice() * getCount();
+    }
 }
