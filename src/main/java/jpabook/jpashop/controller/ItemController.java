6857e74b691a503c7345c011c7a774b23a023a73
origuri <dlcksrb7@naver.com>
2023-07-10 16:15:14 +0900

    2023.7.10 - 주문취소, jpa기본 1편 종료.

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -79,4 +79,5 @@ public class ItemController {
         return "redirect:/items";
 
     }
+
 }
--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -3,13 +3,14 @@ package jpabook.jpashop.controller;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +30,28 @@ public class OrderController {
         model.addAttribute("members", members);
         model.addAttribute("items", items);
         return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") long memberId,
+                        @RequestParam("itemId") long itemId,
+                        @RequestParam("count") int count){
+        // 컨트롤러에서는 필요한 값만 넘겨주고 service에서 entity를 영속상태로 만들어 사용하는 것이 가장 좋다.
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "/order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") long orderId){
+        System.out.println("이거 잘 나옴?-> "+orderId);
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
     }
 }
