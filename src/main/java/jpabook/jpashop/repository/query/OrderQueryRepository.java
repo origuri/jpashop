26ee66382e582a657d2a45ea14e17f030e6bbb9d
origuri <dlcksrb7@naver.com>
2023-07-12 18:51:18 +0900

    2023.7.12 - jpa에서 DTO로 in쿼리를 사용해서 바로 조회하기

--- src/main/java/jpabook/jpashop/repository/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/query/OrderQueryRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,7 +17,7 @@ public class OrderQueryRepository {
 
     public List<OrderQueryDto> findOrderQueryDtos(){
         // 전체 주문을 가저옴.
-        List<OrderQueryDto> result = findOrders();
+        List<OrderQueryDto> result = findOrders(); // n + 1문제 발생
 
         // stream으로 풀어서 orderId로 다시 item을 찾아옴.
         result.stream().forEach(o-> {
@@ -46,5 +48,27 @@ public class OrderQueryRepository {
     }
 
 
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
 
+        // 전체 리스트 뽑은 걸 map으로 orderId 리스트로 매핑해줌. (4, 11)이 들어있음.
+        List<Long> orderIds = result.stream().map(o -> o.getOrderId()).collect(Collectors.toList());
+
+        // orderIds를 파라미터로 in 절로 한번에 orderItems를 가져옴. (4개의 상품.)
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new jpabook.jpashop.repository.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)  " +
+                        "from OrderItem oi " +
+                        "join oi.item i " +
+                        "where oi.order.id in :orderIds",
+                OrderItemQueryDto.class
+        ).setParameter("orderIds", orderIds).getResultList();
+
+        // 그걸 orderId를 키값으로 map에다가 넣어놓음
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream().collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+
+        // order를 for 문으로 풀어서 orderItems에 map의 value(orderId가 키값)을 꺼내서 넣음.
+        result.forEach(orderQueryDto -> orderQueryDto.setOrderItems(orderItemMap.get(orderQueryDto.getOrderId())));
+
+        return result;
+    }
 }
